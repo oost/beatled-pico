bd00e33a84e757f0198f7f45af56d8b29ff0f0a9
Oscar Ostlund <oscar.ostlund@gmail.com>
2023-05-17 18:44:05 -0400

    Update ntohll

--- lib/beatled_protocol/include/beatled/network.h
+++ lib/beatled_protocol/include/beatled/network.h
@@ -4,13 +4,29 @@
 #include <arpa/inet.h>
 
 #ifndef __APPLE__
-#if BYTE_ORDER == BIG_ENDIAN
+
+#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN ||                   \
+    defined(__BIG_ENDIAN__) || defined(__ARMEB__) || defined(__THUMBEB__) ||   \
+    defined(__AARCH64EB__) || defined(_MIBSEB) || defined(__MIBSEB) ||         \
+    defined(__MIBSEB__)
+// Big endian
 #define htonll(x) x
-#else
+
+#elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN ||              \
+    defined(__LITTLE_ENDIAN__) || defined(__ARMEL__) ||                        \
+    defined(__THUMBEL__) || defined(__AARCH64EL__) || defined(_MIPSEL) ||      \
+    defined(__MIPSEL) || defined(__MIPSEL__)
+// Little endian
+
 #define htonll(x) ((((uint64_t)htonl(x)) << 32) + htonl((x) >> 32))
-#endif
-#define ntohll(x) htonll(x)
+
 #else
+#error "I don't know what architecture this is!"
+#endif
+
+// Define the network to hardware equivalent
+#define ntohll(x) htonll(x)
+
 #endif
 
 #endif // BEATLED_PROTOCOL__NETWORK_H
